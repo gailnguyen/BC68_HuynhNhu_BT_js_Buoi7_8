7d33e9f1d20a5655ad97d89084bb45a4bae8fe56
gailnguyen <poshun.cass@gmail.com>
2024-06-02 01:03:16 +0700

    update cau 7

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ document.getElementById("btnThemSo").onclick = function (event) {
   let soN = document.getElementById("soN").value * 1;
   arrSoN.push(soN);
   console.log(arrSoN);
+
   let chuoiSoN = document.createElement("span");
-  chuoiSoN.className = "mt-3 p-2 text-dark";
-  chuoiSoN.innerHTML = arrSoN;
-  document.getElementById("chuoi").appendChild(chuoiSoN);
+  chuoiSoN.className = "mt-3 text-dark";
+  // chuoiSoN.innerHTML = arrSoN;
+  for (let i = 0; i < arrSoN.length; i++) {
+    chuoiSoN.innerHTML = `${arrSoN[i]},`;
+    document.getElementById("chuoi").appendChild(chuoiSoN);
+  }
 };
 
 // 1. Tổng số dương
@@ -97,6 +101,11 @@ document.getElementById("btnDoiViTri").onclick = function () {
   arrSoN[viTriSoThuNhat] = arrSoN[viTriSoThuHai];
   arrSoN[viTriSoThuHai] = soThuNhat;
   console.log(arrSoN);
+
+  let chuoiSoSauDoi = document.createElement("span");
+  chuoiSoSauDoi.className = "mt-3 text-dark";
+  chuoiSoSauDoi.innerHTML = arrSoN;
+  document.getElementById("chuoiDoiSo").appendChild(chuoiSoSauDoi);
 };
 
 // 7. Sắp xếp mảng tăng dần
@@ -105,9 +114,49 @@ document.getElementById("btnSapXepTangDan").onclick = function () {
     return a - b;
   });
   console.log(arrSoN);
+  let chuoiTang = document.createElement("span");
+  chuoiTang.className = "mt-3 text-dark";
+  chuoiTang.innerHTML = arrSoN;
+  document.getElementById("chuoiTang").appendChild(chuoiTang);
 };
 
 // 8. Tìm số nguyên tố đầu tiên trong mảng
+function kiemTraSoNguyenTo(n) {
+  // Boolean flag
+  let flag = true;
+  if (n < 2) {
+    flag = false;
+  } else {
+    for (let i = 2; i < n - 1; i++) {
+      if (n % i == 0) {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  // Kiểm tra biến flag
+  if (flag == true) {
+    return n;
+  } else {
+    console.log(n + " không phải là số nguyên tố");
+  }
+}
+
+let arrSoNguyenTo = [];
+document.getElementById("btnKiemTraSoNguyenTo").onclick = function () {
+  for (let z = 0; z < arrSoN.length; z++) {
+    if (kiemTraSoNguyenTo(arrSoN[z])) {
+      arrSoNguyenTo.push(arrSoN[z]);
+    }
+  }
+  console.log(arrSoNguyenTo);
+  console.log(arrSoNguyenTo[0]);
+  let soNguyenTo = document.createElement("span");
+  soNguyenTo.className = "mt-3 text-dark";
+  soNguyenTo.innerHTML = arrSoNguyenTo[0];
+  document.getElementById("soNguyenTo").appendChild(soNguyenTo);
+};
 
 // 9. Tìm số nguyên
 let arrSoThuc = [];
@@ -115,21 +164,28 @@ document.getElementById("btnThemSoThuc").onclick = function (event) {
   let soThuc = document.getElementById("soThuc").value * 1;
   arrSoThuc.push(soThuc);
   console.log(arrSoThuc);
+
+  let mangSoThuc = document.createElement("span");
+  mangSoThuc.className = "mt-3 text-dark";
+  // mangSoThuc.innerHTML = arrSoN;
+  for (let i = 0; i < arrSoThuc.length; i++) {
+    mangSoThuc.innerHTML = `${arrSoThuc[i]},`;
+    document.getElementById("mangSoThuc").appendChild(mangSoThuc);
+  }
 };
 
-// Hàm kiểm tra số nguyên
-function isInteger(number) {
-  return Number.isInteger(number);
-}
-
+let arrSoNguyen = [];
 document.getElementById("btnDemSoNguyen").onclick = function (event) {
-  let arrSoNguyen = [];
   for (let number of arrSoThuc) {
-    if (isInteger(number)) {
+    if (Number.isInteger(number)) {
       arrSoNguyen.push(number);
     }
   }
   console.log(arrSoNguyen);
+  let mangSoNguyen = document.createElement("span");
+  mangSoNguyen.className = "mt-3 text-dark";
+  mangSoNguyen.innerHTML = arrSoNguyen;
+  document.getElementById("mangSoNguyen").appendChild(mangSoNguyen);
 };
 
 // 10. So sánh số âm dương
@@ -145,9 +201,12 @@ document.getElementById("btnSoSanh").onclick = function (event) {
   }
   if (countSoDuong > countSoAm) {
     console.log("Số dương > Số âm");
+    document.getElementById("soSanhAmDuong").innerHTML = `Số dương > Số âm`;
   } else if (countSoDuong < countSoAm) {
     console.log("Số dương < Số âm");
+    document.getElementById("soSanhAmDuong").innerHTML = `Số dương < Số âm`;
   } else {
     console.log("Số dương = Số âm");
+    document.getElementById("soSanhAmDuong").innerHTML = `Số dương = Số âm`;
   }
 };
